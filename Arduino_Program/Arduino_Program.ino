0f34c05429ea10d4889ec8f47b193107913de6b2
PatzminiHD <7.0@gmx.at>
2022-12-01 17:57:41 +0100

    Add CustomChars and Setup

--- Arduino_Program/Arduino_Program.ino
+++ Arduino_Program/Arduino_Program.ino
@@ -2,8 +2,8 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-Servo servo_upper;
-Servo servo_lower;
+Servo ServoUpper;
+Servo ServoLower;
 
 const int ServoUpperPin = 6;
 const int ServoLowerPin = 5;
@@ -13,10 +13,61 @@ const int ModeButton = 8;
 
 bool prevSwitch = false;
 
-
+byte ModeOff[] = {
+  B01110,
+  B10001,
+  B10001,
+  B10101,
+  B10101,
+  B10001,
+  B10001,
+  B01110
+};
+byte ModeLower[] = {
+  B00100,
+  B00100,
+  B00100,
+  B10101,
+  B01110,
+  B00100,
+  B00000,
+  B00000
+};
+byte ModeUpper[] = {
+  B00000,
+  B00000,
+  B00100,
+  B01110,
+  B10101,
+  B00100,
+  B00100,
+  B00100
+};
+byte ModeAuto[] = {
+  B00000,
+  B01110,
+  B10001,
+  B10001,
+  B10001,
+  B11111,
+  B10001,
+  B10001
+};
 
 void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  ServerLower.attach(ServoLowerPin);
+  ServerUpper.attach(ServoUpperPin);
   
+  pinMode(MCP3008_cs, OUTPUT);
+  pinMode(ModeButton, INPUT);
+  digitalWrite(MCP3008_cs, HIGH);
+  lcd.createChar(0, ModeOff);
+  lcd.createChar(1, ModeAuto);
+  lcd.createChar(2, ModeLower);
+  lcd.createChar(3, ModeUpper);
   
 }
 
