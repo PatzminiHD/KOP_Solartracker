73d8f9abec0e8ad5166c2b21ae3e230d9cebc9fb
PatzminiHD <7.0@gmx.at>
2022-12-01 17:52:22 +0100

    Add Pin numbers

--- Arduino_Program/Arduino_Program.ino
+++ Arduino_Program/Arduino_Program.ino
@@ -1,5 +1,23 @@
-void setup() {
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4);
 
+Servo servo_upper;
+Servo servo_lower;
+
+const int ServoUpperPin = 6;
+const int ServoLowerPin = 5;
+
+const int MCP3008_cs = 10;
+const int ModeButton = 8;
+
+bool prevSwitch = false;
+
+
+
+void setup() {
+  
+  
 }
 
 void loop() {
