9937d0f7e52a7b1b40654602b632a472043d2736
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2023-04-14 21:06:54 +0200

    Add Debug statements

--- Arduino_Program/Arduino_Program.ino
+++ Arduino_Program/Arduino_Program.ino
@@ -211,6 +211,10 @@ void ModeAuto()
     int LDR2Value = mcp3008_read(LCR2Pin);
     int LDR3Value = mcp3008_read(LCR3Pin);
 
+    Serial.print(LDR1Value);
+    Serial.print("\t");
+    Serial.print(LDR3Value);
+    
     //Compare LDR Values for Upper Servo
     if(LDR0Value - ThresholdValue > LDR2Value)
     {
@@ -225,10 +229,12 @@ void ModeAuto()
     if(LDR1Value - ThresholdValue > LDR3Value)
     {
       ServoLowerValue += ServoSteps;
+      Serial.println(">");
     }
     else if(LDR3Value - ThresholdValue > LDR1Value)
     {
       ServoUpperValue -= ServoSteps;
+      Serial.println("<");
     }
 
     //Turn 360° if LowerServo is at its boundary values
